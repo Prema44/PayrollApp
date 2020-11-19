932a5b5fd54792c109c70dff8e6690822710f05a
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-11-19 12:36:20 +0530

    Added : Event Listeners to name element to validate input when document is loaded

--- js/Payroll.js
+++ js/Payroll.js
@@ -9,37 +9,37 @@ class EmployeePayrollData {
         else throw 'Name is incorrect!';
     }
 
-    get id() { return this._id}
+    get id() { return this._id;}
     set id(id) {
         this._id = id;
     }
 
-    get profilePic { return this._profilePic}
+    get profilePic() { return this._profilePic; }
     set profilePic (profilePic){
-        this._profilePic = profilePic
+        this._profilePic = profilePic;
     }
 
-    get gender { return this._gender }
+    get gender() { return this._gender; }
     set gender (gender) {
-        this._gender = gender
+        this._gender = gender;
     }
 
-    get salary() { return this._salary}
+    get salary() { return this._salary;}
     set salary (salary) {
-        this._salary = salary
+        this._salary = salary;
     }
 
-    get department() { return this._department }
+    get department() { return this._department; }
     set department (department) {
         this._department = department ;
     }
 
-    get notes() { return this._notes }
+    get notes() { return this._notes; }
     set notes (notes) {
         this._notes = notes;
     }
 
-    get startDate() { return this._startDate}
+    get startDate() { return this._startDate; }
     set startDate(startDate){
         this._startDate = startDate;
     }
@@ -57,11 +57,11 @@ class EmployeePayrollData {
 const createEmployeePayrollData = () => {
     let employeePayrollData = new EmployeePayrollData();
     try{
-        employeePayrollData.name = getInputValueById('#name')
+        employeePayrollData.name = getInputValueById('#name');
     }
     catch(e){
         setTextValue('.text-error', e);
-        throw e
+        throw e;
     }
     employeePayrollData.salary = getInputValueById('#salary');
     let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
@@ -76,24 +76,28 @@ const getInputValueById = (id) => {
     return value;
 }
 
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
-    output.textContent = salary.value;
-});
+window.addEventListener('DOMContentLoaded', () => {
 
-const textError = document.querySelector('.text-error');
-const name = document.querySelector('#name');
-name.addEventListener('input', function(){
-    if(name.value.length == 0){
-        textError.textContent = "";
-        return;
-    }
-    try{
-        (new EmployeePayrollData()).name = name.value;
-        textError.textContent = "";
-    }catch(e){
-        textError.textContent = e;
-    }
+    const textError = document.querySelector('.text-error');
+    const name = document.querySelector('#name');
+    name.addEventListener('input', function(){
+        if(name.value.length == 0){
+            textError.textContent = "";
+            return;
+        }
+        try{
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        }catch(e){
+            textError.textContent = e;
+        }
+    });
+
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    });
+
 });
